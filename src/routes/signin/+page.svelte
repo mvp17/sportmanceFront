4fcf8b52586fb3ff88013fedd3b0423fa98174b4
mvp17 <mvisapascual@gmail.com>
2023-11-01 20:46:11 +0100

    Add api call for sign up and sign in.

--- src/routes/signin/+page.svelte
+++ src/routes/signin/+page.svelte
@@ -4,18 +4,34 @@
 
 <script>
   import { Button, Card, Label, Input } from 'flowbite-svelte';
+  import axios from 'axios'
+
+	let /** @type {string} */ email, /** @type {string} */ password;
+	const baseURL = 'http://localhost:8000/api/'
+	
+	async function signin () {
+		try {
+			const res = await axios.post(baseURL + 'login', { email: email, password: password });
+      const token = res.data
+			console.log(token)
+		} catch (err) {
+			console.log(err);
+		}		
+	}
 </script>
 
 <div class="text-column">
   <Card>
-    <div class="mb-6">
-      <Label for="email" class="mb-2">Email address</Label>
-      <Input type="email" id="email" placeholder="john.doe@company.com" required />
-    </div>
-    <div class="mb-6">
-      <Label for="password" class="mb-2">Password</Label>
-      <Input type="password" id="password" placeholder="•••••••••" required />
-    </div>
-    <Button type="submit">Sign In</Button>
+    <form on:submit|preventDefault={signin}>
+      <div class="mb-6">
+        <Label for="email" class="mb-2">Email address</Label>
+        <Input type="email" id="email" placeholder="john.doe@company.com" bind:value={email} required />
+      </div>
+      <div class="mb-6">
+        <Label for="password" class="mb-2">Password</Label>
+        <Input type="password" id="password" placeholder="•••••••••" bind:value={password} required />
+      </div>
+      <Button type="submit">Sign In</Button>
+    </form>
   </Card>
 </div>
--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -3,23 +3,38 @@
 </svelte:head>
 
 <script>
-  import { Button, Card, Label, Input } from 'flowbite-svelte';
+	import { Button, Card, Label, Input } from 'flowbite-svelte';
+	import axios from 'axios'
+	
+	let /** @type {string} */username, /** @type {string} */ email, /** @type {string} */ password;
+	const baseURL = 'http://localhost:8000/api/'
+	
+	async function signup () {
+		try {
+			const res = await axios.post(baseURL + 'register', { name: username, email: email, password: password });
+			console.log(res.data)
+		} catch (err) {
+			console.log(err);
+		}		
+	}
 </script>
 
 <div class="text-column">
   <Card>
+	<form on:submit|preventDefault={signup}>
 		<div class="mb-6">
-      <Label for="username" class="mb-2">Username</Label>
-      <Input type="text" id="username" placeholder="John" required />
-    </div>
+		<Label for="username" class="mb-2">Username</Label>
+		<Input type="text" id="username" placeholder="John" bind:value={username} required />
+		</div>
+			<div class="mb-6">
+			<Label for="email" class="mb-2">Email address</Label>
+			<Input type="email" id="email" placeholder="john.doe@company.com" bind:value={email} required />
+		</div>
 		<div class="mb-6">
-    	<Label for="email" class="mb-2">Email address</Label>
-    	<Input type="email" id="email" placeholder="john.doe@company.com" required />
-  	</div>
-  	<div class="mb-6">
-    	<Label for="password" class="mb-2">Password</Label>
-    	<Input type="password" id="password" placeholder="•••••••••" required />
-  	</div>
-    <Button type="submit">Sign Up</Button>
+			<Label for="password" class="mb-2">Password</Label>
+			<Input type="password" id="password" placeholder="•••••••••" bind:value={password} required />
+		</div>
+		<Button type="submit">Sign Up</Button>
+	</form>
   </Card>
 </div>
