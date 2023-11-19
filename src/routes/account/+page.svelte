b50a4628357e91b9a6c8b99a45eebeb57a778a91
mvp17 <mvisapascual@gmail.com>
2023-11-19 16:38:16 +0100

    Account view and signout view virtually DONE.

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -3,13 +3,34 @@
 </svelte:head>
 
 <script>
-    import { Button, Card } from 'flowbite-svelte';
-    let user = { username: "Marc" };
+    import axios from 'axios';
+    import { Card } from 'flowbite-svelte';
+    import { onMount } from 'svelte';
+    import { baseURL } from '../../environment';
+    
+    let name = "";
+    let email = "";
+    
+    onMount (async () => {
+        try {
+            axios.defaults.withCredentials = true;
+            const instance = axios.create({ baseURL: baseURL });
+            const res = await instance.get('/user');
+            name = res.data.name;
+            email = res.data.email;
+		} catch (err) {
+			console.log(err);
+		}
+    });
 </script>
     
+
+
 <div class="text-column">
-    <Card>
-        <h2>Do you want to delete your account?</h2>
-        <Button color="red">Delete Account</Button>
+    <Card padding="sm">
+        <div class="flex flex-col items-center pb-4">
+          <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{email}</h5>
+          <span class="text-sm text-gray-500 dark:text-gray-400">{name}</span>
+        </div>
     </Card>
 </div>
--- src/routes/signout/+page.svelte
+++ src/routes/signout/+page.svelte
@@ -3,24 +3,66 @@
 </svelte:head>
 
 <script>
+  import axios from 'axios';
   import { Button, Card } from 'flowbite-svelte';
-	let isThereConfiguration = 1;
+  import { onMount } from 'svelte';
+  import { baseURL } from '../../environment';
+  import { jwt } from '../../stores/sessionStore';
+  import { goto } from '$app/navigation';
+	
+  let isThereConfiguration = 0;
+
+  onMount (async () => {
+    try {
+      axios.defaults.withCredentials = true;
+      const instance = axios.create({ baseURL: baseURL });
+      const res = await instance.get('/exit-session');
+      isThereConfiguration = res.data.is_there_settings;
+		} catch (err) {
+			console.log(err);
+		}
+  });
+
+  async function deleteSessionData () {
+    try {
+      axios.defaults.withCredentials = true;
+      const instance = axios.create({ baseURL: baseURL });
+      const res = await instance.post('/delete-session-data');
+		} catch (err) {
+			console.log(err);
+		}
+  }
+
+  async function signOut () {
+    try {
+      axios.defaults.withCredentials = true;
+      const instance = axios.create({ baseURL: baseURL });
+      const res = await instance.post('/logout');
+      jwt.set('');
+      goto('/')
+		} catch (err) {
+			console.log(err);
+		}
+  }
 </script>
 
 <div class="text-column">
   <Card>
     {#if isThereConfiguration === 1}
       <h2>
-          Do you want to delete your session for a new analysis?
+          Do you want to delete keywords and settings of this session?
       </h2>
+      <Button color="red" style="margin-top:10px" on:click={deleteSessionData}> Delete </Button>
+    {:else}
       <h2>
-          Your key words and your configuration settings.
+        There is no session data registered.
       </h2>
-      <Button color="red"> Delete </Button>
     {/if}
+  </Card>
+  <Card style="margin-top:20px">
     <h2>
-      Are you sure you want to leave ?
+      Are you sure you want to leave?
     </h2>
-    <Button color="green"> Log Out </Button>
+    <Button color="green" style="margin-top:10px" on:click={signOut}> Log Out </Button>
   </Card>
 </div>
